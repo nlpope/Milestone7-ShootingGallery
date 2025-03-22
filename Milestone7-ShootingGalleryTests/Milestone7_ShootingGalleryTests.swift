8c9046236a610fb75aec6eb4be62664fadfdf259
Noah Pope <npope@tutanota.com>
2025-03-22 09:04:35 -0500

    Initial Commit

---
+++ Milestone7-ShootingGalleryTests/Milestone7_ShootingGalleryTests.swift
@@ -0,0 +1,14 @@
+//  File: Milestone7_ShootingGalleryTests.swift
+//  Project: Milestone7-ShootingGalleryTests
+//  Created by: Noah Pope on 3/22/25.
+
+import Testing
+@testable import Milestone7_ShootingGallery
+
+struct Milestone7_ShootingGalleryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
